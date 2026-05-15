5a4f19b5fce26e2573eefd9ccf505b687e61c122
DuyAnh <lotdanguoitinh@gmail.com>
2026-05-16 00:20:53 +0700

    Build simulator

--- include/SimulatorMocks.h
+++ include/SimulatorMocks.h
@@ -15,3 +15,50 @@ class SerialMock {
   void println(const char* s) { printf("%s\n", s); }
 };
 static SerialMock Serial;
+
+// #pragma once
+// #ifdef SIMULATOR
+// #include <stdint.h>
+// #include <string>
+// #include <atomic>
+//
+// // --- 1. GIẢ LẬP CROSSPOINTWEBSERVER ---
+// class CrossPointWebServer {
+// public:
+//   CrossPointWebServer() {}
+//   ~CrossPointWebServer() {}
+//   void begin() {}
+//   void stop() {}
+//   void handleClient() {}
+//   bool getWsUploadStatus() const { return false; }
+// };
+//
+// // --- 2. GIẢ LẬP OTAUPDATER ---
+// namespace OtaUpdater {
+//   inline void checkForUpdate() {}
+//   inline bool isUpdateNewer() const { return false; }
+//   inline std::string getLatestVersion() const { return "1.0.0-sim"; }
+//   inline void installUpdate(void (*callback)(void*), void* arg, std::atomic<bool>* cancel) {}
+// }
+//
+// // --- 3. GIẢ LẬP FIRMWARE_FLASH ---
+// namespace firmware_flash {
+//   enum Result {
+//     SUCCESS = 0,
+//     ERROR_FILE_NOT_FOUND,
+//     ERROR_INVALID_IMAGE
+//   };
+//
+//   inline Result validateImageFile(const char* path, unsigned long size) {
+//     return SUCCESS;
+//   }
+//
+//   inline const char* resultName(Result r) {
+//     return "SIM_SUCCESS";
+//   }
+//
+//   inline Result flashFromSdPath(const char* path, void (*progress)(unsigned long, unsigned long, void*), void* arg, bool flag) {
+//     return SUCCESS;
+//   }
+// }
+// #endif
